a7211c90f7ef2799cdfbd86a792aae36b46357a9
Tapan Parmar <tapan1359@gmail.com>
2019-09-04 13:59:31 -0400

    checking readme

--- src/index.js
+++ src/index.js
@@ -13,27 +13,39 @@ const publicDirPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirPath))
 
+// Listen to connection event
 io.on('connection', (socket) => {
     console.log('new websocket connection')
 
+    // emit welcome message
     socket.emit('message', 'Welcome')
+    // Emit mesaage to all users but joined one
     socket.broadcast.emit('message', 'A new user has joined')
 
+    // list to the event sendMessage from chat.js
     socket.on('sendMessage', (message, callback) => {
         const filter = new Filter()
         if (filter.isProfane(message)) {
             return callback('Profanity is not allowed')
         }
+
+        // Emit message event to all connected users
         io.emit('message', message)
         callback()
     })
 
+    // Listen to disconnect event
     socket.on('disconnect', () => {
+        // Emit message to all the user
         io.emit('message', 'User has left')
     })
 
-    socket.on('sendLocation', (position) => {
-        io.emit('message', `https://google.com/maps/?q=${position.latitude},${position.longitude}`)
+    // Location
+    // Listen to event sendLocation
+    socket.on('sendLocation', (position, callback) => {
+        // Emit message to all user with google location link
+        io.emit('locationMessage', `https://google.com/maps/?q=${position.latitude},${position.longitude}`)
+        callback()
     })
 })
 
